d480568b682e18fdc45c27b7337cce79c029eba4
now_0o <dlwpdn0704@gmail.com>
2023-04-02 23:06:05 +0900

    경도/위도 가져오기, openweather에서 날씨 받아오기

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import Loading from './Loading';
+import * as Location from 'expo-location'
+import { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
+import axios from 'axios';
+
+const API_KEY = "75048e0afbb4b810d242ea7e0a0d38fe";
 
 export default function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  getWeather = async (latitude, longitude)=>{
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    console.log(data);
+  }
+  getLocation = async ()=>{
+    try{
+      await Location.requestBackgroundPermissionsAsync();
+      const {coords : { latitude, longitude }} = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      getWeather(latitude, longitude);
+      setIsLoading(false);
+    }catch(err){
+      Alert.alert("사용자 위치를 찾을 수 없습니다. 위치 정보를 허용해 주세요.")
+    }
+    
+  }
+  useEffect(()=>{
+    getLocation()
+  }, [])
   return (
-    <Loading />
+    isLoading ? <Loading /> : null
   );
 }
 
